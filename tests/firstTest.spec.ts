9fe54d51d505ba04b335c5ebb0206f034ceb8134
yashy_ <88859091+Asha-Coding@users.noreply.github.com>
2025-11-01 20:30:33 -0600

    Adding Tests

---
+++ tests/firstTest.spec.ts
@@ -0,0 +1,48 @@
+import { expect, test } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("http://localhost:4200/");
+});
+
+test.only('Turn on Dark Theme', async ({ page }) => {
+
+  await page.locator("nb-select[ng-reflect-selected='default'].appearance-outline.size-medium").click();
+  await page.locator("nb-option#nb-option-1.nb-transition.ng-star-inserted").click();
+  
+});
+
+test.describe("Charts", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByText("Charts", { exact: true }).click();
+  });
+
+  test("Go to ECharts", async ({ page }) => {
+    await page.getByText("Echarts").click();
+  });
+});
+
+test.describe("Forms", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByText("Forms").click();
+    await page.getByText("Form Layouts").click();
+  });
+
+  test("Fill Email", async ({ page }) => {
+    await page
+      .locator('input[placeholder="Email"]#inputEmail1.input-full-width')
+      .fill("test@gmail.com");
+  });
+});
+
+test.describe("Dialogs Names", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByText("Modal & Overlays", { exact: true }).click();
+    await page.getByText("Dialog").click();
+  });
+
+  test("Fill Dialog Names", async ({ page }) => {
+    await page.locator("button:has-text('Enter Name').appearance-filled.size-medium.shape-rectangle.status-basic.nb-transition").click();
+    await page.locator("input[placeholder='Name'].size-medium.status-basic.shape-rectangle.nb-transition").fill("Bob Smith");
+    await page.locator("button:has-text('Submit').status-success.appearance-filled.size-medium.shape-rectangle.nb-transition").click();
+  });
+});
